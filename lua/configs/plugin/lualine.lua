8bf11544b70598b18dceda7c2c408e7be919168b
MysticalDevil <jinpeng7981@outlook.com>
2023-01-24 22:55:37 +0800

    Change lualine config

--- lua/configs/plugin/lualine.lua
+++ lua/configs/plugin/lualine.lua
@@ -47,6 +47,26 @@ local diagnostics = {
   },
 }
 
+local filename = {
+  "filename",
+  file_status = true, -- Displays file status (readonly status, modified status)
+  newfile_status = true, -- Display new file status (new file means no write after created)
+  path = 1, -- 0: Just the filename
+  -- 1: Relative path
+  -- 2: Absolute path
+  -- 3: Absolute path, with tilde as the home directory
+
+  shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+
+  -- for other components. (terrible name, any suggestions?)
+  symbols = {
+    modified = "[*]", -- Text to show when the file is modified.
+    readonly = " ", -- Text to show when the file is non-modifiable or readonly.
+    unnamed = "[No Name]", -- Text to show for unnamed buffers.
+    newfile = "[New]", -- Text to show for new created file before first writting
+  },
+}
+
 local opts = {
   options = {
     theme = "onedark",
@@ -62,7 +82,7 @@ local opts = {
   extensions = { "nvim-tree", "toggleterm" },
   sections = {
     lualine_c = {
-      "filename",
+      filename,
       {
         "lsp_progress",
         spinner_symbols = { "", "", "", "", "", "" },
