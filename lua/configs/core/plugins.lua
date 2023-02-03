50bae0b818a77028a2bac1fb0d24a0c6a0850561
MysticalDevil <jinpeng7981@outlook.com>
2023-02-03 22:13:42 +0800

    Add lazy.nvim config option

--- lua/configs/core/plugins.lua
+++ lua/configs/core/plugins.lua
@@ -664,11 +664,17 @@ lazy.setup({
       require("dap.vimspector")
     end,
   },
+}, {
+  checker = {
+    -- automatically check for plugin updates
+    enabled = true,
+    concurrency = nil, ---@type number? set to 1 to check for updates very slowly
+    notify = true, -- get a notification when new updates are found
+    frequency = 3600, -- check for updates every hour
+  },
+  change_detection = {
+    -- automatically check for config file changes and reload the ui
+    enabled = true,
+    notify = true, -- get a notification when changes are found
+  },
 })
--- 每次保存 plugins.lua 自动安装插件
--- vim.cmd([[
---     augroup packer_user_config
---     autocmd!
---     autocmd BufWritePost plugins.lua source <afile>  | PackerSync
---     augroup end
---   ]])
