f105705e75c3b74179ea6c7a5dc57eadc56c4e13
MysticalDevil <jinpeng7981@outlook.com>
2023-03-26 20:41:42 +0800

    Some change

--- lua/cmp/setup.lua
+++ lua/cmp/setup.lua
@@ -52,6 +52,10 @@ cmp.setup({
   -- 来源
   sources = cmp.config.sources({
     {
+      name = "luasnip",
+      group_index = 1,
+    },
+    {
       name = "nvim_lsp",
       group_index = 1,
     },
@@ -59,10 +63,6 @@ cmp.setup({
     --   name = "nvim_lsp_signature_help",
     --   group_index = 1,
     -- },
-    {
-      name = "luasnip",
-      group_index = 1,
-    },
     {
       name = "buffer",
       group_index = 2,
