946691e825ad2f834d7db317e6350fad005a7fe6
MysticalDevil <jinpeng7981@outlook.com>
2023-04-09 16:11:04 +0800

    Add lsp-zero.nvim plugin

--- lua/configs/core/plugins.lua
+++ lua/configs/core/plugins.lua
@@ -570,6 +570,28 @@ local list = {
       require("configs.plugin.neodev")
     end,
   },
+  -- lsp-zero.nvim
+  -- A starting point to setup some lsp related features in neovim.
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v2.x",
+    dependencies = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" }, -- Required
+      { -- Optional
+        "williamboman/mason.nvim",
+        build = function()
+          pcall(vim.cmd, "MasonUpdate")
+        end,
+      },
+      { "williamboman/mason-lspconfig.nvim" }, -- Optional
+
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" }, -- Required
+      { "hrsh7th/cmp-nvim-lsp" }, -- Required
+      { "L3MON4D3/LuaSnip" }, -- Required
+    },
+  },
   -- null-ls.nvim
   -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more
   {
