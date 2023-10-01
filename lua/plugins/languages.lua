a8f383975fc17a057229fd098ef2bf15577392bf
MysticalDevil <jinpeng7981@outlook.com>
2023-10-01 19:56:03 +0800

    Add lsp-lens.nvim plugin

--- lua/plugins/languages.lua
+++ lua/plugins/languages.lua
@@ -596,4 +596,24 @@ return {
       })
     end,
   },
+  -- lsp-lens.nvim
+  --
+  {
+    "VidocqH/lsp-lens.nvim",
+    cmd = { "LspLensOn", "LspLensOff", "LspLensToggle" },
+    config = function()
+      require("lsp-lens").setup({
+        enable = true,
+        include_declaration = false, -- Reference include declaration
+        sections = { -- Enable / Disable specific request
+          definition = false,
+          references = true,
+          implements = true,
+        },
+        ignore_filetype = {
+          "prisma",
+        },
+      })
+    end,
+  },
 }
