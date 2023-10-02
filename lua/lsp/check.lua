20b9aa72ac987a6e5f712425a5e616bb62207c53
MysticalDevil <jinpeng7981@outlook.com>
2023-10-02 16:36:00 +0800

    Use pylyzer to replace pyright(for inlay hints)

--- lua/lsp/check.lua
+++ lua/lsp/check.lua
@@ -26,11 +26,11 @@ if util.check_os() == "Linux" then
   table.insert(required_npm, "bashls")
 end
 
-local required_cargo = { "fennel_language_server", "neocmake" }
+local required_cargo = { "fennel_language_server", "neocmake", "pylyzer" }
 
 local required_go = { "efm", "gopls" }
 
-local required_pip = { "cmake", "pyright" }
+local required_pip = { "cmake" }
 
 local required_gem = { "solargraph" }
 
