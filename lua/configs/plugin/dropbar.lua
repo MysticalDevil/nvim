36e5d4d20229d8a74a05ac68bcde2f821acf6a04
MysticalDevil <jinpeng7981@outlook.com>
2023-10-05 18:50:49 +0800

    Fix dropbar.nvim's roblems

--- lua/configs/plugin/dropbar.lua
+++ lua/configs/plugin/dropbar.lua
@@ -14,6 +14,7 @@ local opts = {
     enable = function(buf, win)
       return not vim.api.nvim_win_get_config(win).zindex
         and vim.bo[buf].buftype == ""
+        and vim.bo[buf].filetype ~= ""
         and vim.api.nvim_buf_get_name(buf) ~= ""
         and not vim.wo[win].diff
     end,
