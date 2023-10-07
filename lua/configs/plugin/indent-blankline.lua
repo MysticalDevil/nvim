265600b85ded5d2dfbaf968e335fdb6b68d3f2cf
MysticalDevil <jinpeng7981@outlook.com>
2023-10-07 08:53:06 +0800

    Adjust indent-blankline.nvim config, hide first indent level

--- lua/configs/plugin/indent-blankline.lua
+++ lua/configs/plugin/indent-blankline.lua
@@ -30,6 +30,11 @@ vim.g.rainbow_delimiters = { highlight = highlight }
 
 hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
 
+-- hide first line indent
+hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+
+-- ibl config
 local opts = {
   debounce = 100,
   viewport_buffer = {
