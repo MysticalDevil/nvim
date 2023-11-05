8cc8f4829448fda5152bdc739f5fc8f4fc724e3d
Mystical-C <64483945+MysticalDevil@users.noreply.github.com>
2023-11-05 13:15:56 +0800

    fix: Update neogit config

--- lua/devil/configs/plugin/neogit.lua
+++ lua/devil/configs/plugin/neogit.lua
@@ -19,6 +19,18 @@ local opts = {
   -- `false` will change nvim to insert mode, and `"auto"` will change nvim to insert mode
   -- IF the commit message is empty, otherwise leaving it in normal mode.
   disable_insert_on_commit = true,
+  -- When enabled, will watch the `.git/` directory for changes and refresh the status buffer in response to filesystem
+  -- events.
+  filewatcher = {
+    interval = 1000,
+    enabled = true,
+  },
+  -- Used to generate URL's for branch popup action "pull request".
+  git_services = {
+    ["github.com"] = "https://github.com/${owner}/${repository}/compare/${branch_name}?expand=1",
+    ["bitbucket.org"] = "https://bitbucket.org/${owner}/${repository}/pull-requests/new?source=${branch_name}&t=1",
+    ["gitlab.com"] = "https://gitlab.com/${owner}/${repository}/merge_requests/new?merge_request[source_branch]=${branch_name}",
+  },
   -- Allows a different telescope sorter. Defaults to 'fuzzy_with_index_bias'. The example below will use the native fzf
   -- sorter instead. By default, this function returns `nil`.
   telescope_sorter = function()
@@ -50,36 +62,20 @@ local opts = {
   console_timeout = 2000,
   -- Automatically show console if a command takes more than console_timeout milliseconds
   auto_show_console = true,
-  status = {
-    recent_commit_count = 10,
-  },
-  commit_editor = {
-    kind = "split",
-  },
-  commit_select_view = {
-    kind = "tab",
-  },
+  status = { recent_commit_count = 10 },
+  commit_editor = { kind = "split" },
+  commit_select_view = { kind = "tab" },
   commit_view = {
     kind = "vsplit",
+    verify_commit = os.execute("which gpg") == 0, -- Can be set to true or false, otherwise we try to find the binary
   },
-  log_view = {
-    kind = "tab",
-  },
-  rebase_editor = {
-    kind = "split",
-  },
-  reflog_view = {
-    kind = "tab",
-  },
-  merge_editor = {
-    kind = "split",
-  },
-  preview_buffer = {
-    kind = "split",
-  },
-  popup = {
-    kind = "split",
-  },
+  log_view = { kind = "tab" },
+  rebase_editor = { kind = "split" },
+  reflog_view = { kind = "tab" },
+  merge_editor = { kind = "split" },
+  tag_editor = { kind = "split" },
+  preview_buffer = { kind = "split" },
+  popup = { kind = "split" },
   signs = {
     -- { CLOSED, OPENED }
     hunk = { "", "" },
@@ -90,12 +86,12 @@ local opts = {
   integrations = {
     -- If enabled, use telescope for menu selection rather than vim.ui.select.
     -- Allows multi-select and some things that vim.ui.select doesn't.
-    telescope = nil,
+    telescope = true,
     -- Neogit only provides inline diffs. If you want a more traditional way to look at diffs, you can use `diffview`.
     -- The diffview integration enables the diff popup.
     --
     -- Requires you to have `sindrets/diffview.nvim` installed.
-    diffview = nil,
+    diffview = true,
 
     -- If enabled, uses fzf-lua for menu selection. If the telescope integration
     -- is also selected then telescope is used instead
@@ -104,50 +100,17 @@ local opts = {
   },
   sections = {
     -- Reverting/Cherry Picking
-    sequencer = {
-      folded = false,
-      hidden = false,
-    },
-    untracked = {
-      folded = false,
-      hidden = false,
-    },
-    unstaged = {
-      folded = false,
-      hidden = false,
-    },
-    staged = {
-      folded = false,
-      hidden = false,
-    },
-    stashes = {
-      folded = true,
-      hidden = false,
-    },
-    unpulled_upstream = {
-      folded = true,
-      hidden = false,
-    },
-    unmerged_upstream = {
-      folded = false,
-      hidden = false,
-    },
-    unpulled_pushRemote = {
-      folded = true,
-      hidden = false,
-    },
-    unmerged_pushRemote = {
-      folded = false,
-      hidden = false,
-    },
-    recent = {
-      folded = true,
-      hidden = false,
-    },
-    rebase = {
-      folded = true,
-      hidden = false,
-    },
+    sequencer = { folded = false, hidden = false },
+    untracked = { folded = false, hidden = false },
+    unstaged = { folded = false, hidden = false },
+    staged = { folded = false, hidden = false },
+    stashes = { folded = true, hidden = false },
+    unpulled_upstream = { folded = true, hidden = false },
+    unmerged_upstream = { folded = false, hidden = false },
+    unpulled_pushRemote = { folded = true, hidden = false },
+    unmerged_pushRemote = { folded = false, hidden = false },
+    recent = { folded = true, hidden = false },
+    rebase = { folded = true, hidden = false },
   },
   mappings = {
     finder = {
@@ -163,6 +126,23 @@ local opts = {
       ["<c-j>"] = "NOP",
     },
     -- Setting any of these to `false` will disable the mapping.
+    popup = {
+      ["?"] = "HelpPopup",
+      ["A"] = "CherryPickPopup",
+      ["D"] = "DiffPopup",
+      ["M"] = "RemotePopup",
+      ["P"] = "PushPopup",
+      ["X"] = "ResetPopup",
+      ["Z"] = "StashPopup",
+      ["b"] = "BranchPopup",
+      ["c"] = "CommitPopup",
+      ["f"] = "FetchPopup",
+      ["l"] = "LogPopup",
+      ["m"] = "MergePopup",
+      ["p"] = "PullPopup",
+      ["r"] = "RebasePopup",
+      ["v"] = "RevertPopup",
+    },
     status = {
       ["q"] = "Close",
       ["I"] = "InitRepo",
@@ -185,21 +165,6 @@ local opts = {
       ["<c-v>"] = "VSplitOpen",
       ["<c-x>"] = "SplitOpen",
       ["<c-t>"] = "TabOpen",
-      ["?"] = "HelpPopup",
-      ["D"] = "DiffPopup",
-      ["p"] = "PullPopup",
-      ["r"] = "RebasePopup",
-      ["m"] = "MergePopup",
-      ["P"] = "PushPopup",
-      ["c"] = "CommitPopup",
-      ["l"] = "LogPopup",
-      ["v"] = "RevertPopup",
-      ["Z"] = "StashPopup",
-      ["A"] = "CherryPickPopup",
-      ["b"] = "BranchPopup",
-      ["f"] = "FetchPopup",
-      ["X"] = "ResetPopup",
-      ["M"] = "RemotePopup",
       ["{"] = "GoToPreviousHunkHeader",
       ["}"] = "GoToNextHunkHeader",
     },
