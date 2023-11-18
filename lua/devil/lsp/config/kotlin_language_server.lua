b4224998c97d994b8bf6aa3c70fb5e3e4b658260
Mystical-C <64483945+MysticalDevil@users.noreply.github.com>
2023-11-18 16:52:07 +0800

    feat(lsp): Add kotlin-language-server inlay hints support

--- lua/devil/lsp/config/kotlin_language_server.lua
+++ lua/devil/lsp/config/kotlin_language_server.lua
@@ -3,6 +3,16 @@ local util = require("devil.lsp.util")
 
 local opts = util.default_configs()
 
+opts.settings = {
+  kotlin = {
+    hints = {
+      typeHints = true,
+      parameterHints = true,
+      chaineHints = true,
+    },
+  },
+}
+
 opts.filetypes = { "kotlin" }
 opts.root_dir = function(fname)
   return lsp_util.root_pattern("settings.gradle", "settings.gradle.kts")(fname)
