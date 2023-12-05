cc5f0eb94907cf70804aec06327647d16ecaf4fb
Mystical-C <64483945+MysticalDevil@users.noreply.github.com>
2023-12-05 13:31:54 +0800

    feat(tree-sitter): Add fsharp support

--- lua/devil/configs/plugin/nvim-treesitter.lua
+++ lua/devil/configs/plugin/nvim-treesitter.lua
@@ -170,3 +170,14 @@ parser_config.org = { ---@diagnostic disable-line
   },
   filetype = "org",
 }
+
+parser_config.fsharp = { ---@diagnostic disable-line
+  install_info = {
+    url = "https://github.com/Nsidorenco/tree-sitter-fsharp",
+    branch = "develop",
+    files = { "src/scanner.cc", "src/parser.c" },
+    generate_requires_npm = true,
+    requires_generate_from_grammer = true,
+  },
+  filetype = "fsharp",
+}
