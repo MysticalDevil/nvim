7d86b6f3fb64f05b6c6640bf0a1e6a56e8444e1f
Mystical-C <64483945+MysticalDevil@users.noreply.github.com>
2024-02-24 23:59:04 +0800

    feat: Add some new gitsigns.nvim keymaps

--- lua/devil/core/mappings.lua
+++ lua/devil/core/mappings.lua
@@ -333,7 +333,7 @@ M.gitsigns = {
 
     ["<leader>gb"] = {
       function()
-        package.loaded.gitsigns.blame_line()
+        require("gitsigns").blame_line()
       end,
       "Blame line",
     },
@@ -343,6 +343,21 @@ M.gitsigns = {
         require("gitsigns").toggle_deleted()
       end,
       "Toggle deleted",
+    },
+
+    ["<leader>tl"] = {
+      function()
+        require("gitsigns").toggle_numhl()
+        require("gitsigns").toggle_linehl()
+      end,
+      "Toggle line hightlight",
+    },
+
+    ["<leader>tw"] = {
+      function()
+        require("gitsigns").toggle_word_diff()
+      end,
+      "Toggle different word",
     },
   },
 }
--- lua/devil/plugins/configs/gitsigns.lua
+++ lua/devil/plugins/configs/gitsigns.lua
@@ -12,11 +12,12 @@ return {
   -- line number highlight
   numhl = false, -- Toggle with `Gitsigns toggle_numhl`
   linehl = false, -- Toggle with `Gitsigns toggle_linehl`
-  word_diff = true, -- Toggle with `Gitsigns toggle_word_diff`
+  word_diff = false, -- Toggle with `Gitsigns toggle_word_diff`
   watch_gitdir = {
     interval = 1000,
     follow_files = true,
   },
+  auto_attach = true,
   attach_to_untracked = true,
   current_line_blame = false, -- Toggle with `LGitsigns toggle_current_line_blame`
   current_line_blame_opts = {
@@ -24,6 +25,7 @@ return {
     virt_text_pos = "eol", -- 'eol' | 'overlat' | 'right_align'
     delay = 1000,
     ignore_whitespace = false,
+    virt_text_priority = 100,
   },
   current_line_blame_formatter_opts = {
     relative_time = false,
