dd9392ed6d8e2a80e3466aef567e2ac0883a8aaf
Mystical-C <64483945+MysticalDevil@users.noreply.github.com>
2024-06-10 13:08:02 +0800

    fix: Fix python semshi plugin config

--- lua/devil/plugins/list/prog.lua
+++ lua/devil/plugins/list/prog.lua
@@ -170,11 +170,13 @@ return {
     "wookayin/semshi", -- use a maintained fork
     ft = "python",
     build = ":UpdateRemotePlugins",
+    version = "*",
     init = function()
       -- Disabled these features better provided by LSP or other more general plugins
       vim.g["semshi#error_sign"] = false
       vim.g["semshi#simplify_markup"] = false
-      -- vim.g["semshi#mark_selected_nodes"] = false
+      vim.g["semshi#mark_selected_nodes"] = false
+      vim.g["semshi#update_delay_factor"] = 0.001
 
       -- This autocmd must be defined in init to take effect
       vim.api.nvim_create_autocmd({ "VimEnter", "ColorScheme" }, {
@@ -182,18 +184,21 @@ return {
         callback = function()
           -- Only add style, inherit or link to the LSP's colors
           vim.cmd([[
-            highlight! semshiGlobal gui=italic
-            highlight! semshiImported gui=bold
+            highlight! link semshiGlobal  @none
+            highlight! link semshiImported @none
             highlight! link semshiParameter @lsp.type.parameter
-            highlight! link semshiParameterUnused DiagnosticUnnecessary
             highlight! link semshiBuiltin @function.builtin
-            highlight! link semshiAttribute @attribute
+            highlight! link semshiAttribute @field
             highlight! link semshiSelf @lsp.type.selfKeyword
-            highlight! link semshiUnresolved @lsp.type.unresolvedReference
+            highlight! link semshiUnresolved @none
+            highlight! link semshiFree @none
+            highlight! link semshiAttribute @none
+            highlight! link semshiParameterUnused @none
             ]])
         end,
       })
     end,
+    config = function() end,
   },
 
   --------------------- Go ----------------------
