e5c284b488c6410bbad138fc3501e288f7ce79f5
MysticalDevil <jinpeng7981@outlook.com>
2026-01-01 10:44:14 +0800

    feat: add two new autocmd
    Changes:
    - Feat: Added highlight on yank autocmd, added goto lastlocate when
      opening a buffer.

--- lua/devil/core/autocmds.lua
+++ lua/devil/core/autocmds.lua
@@ -1,19 +1,19 @@
 local autocmd = vim.api.nvim_create_autocmd
 local augroup = vim.api.nvim_create_augroup
 
-local commonAutoGroup = augroup("commonAutoGroup", { clear = true })
+local commanAugroup = augroup("commonAugroup", { clear = true })
 -- local writeAutoGroup = augroup("writeAutoGroup", { clear = true })
 
 local lispFiletypes = { "clj", "*.el", "*.fnl", "*.hy", "*.janet", "*.lisp", "*.rkt", "*.scm" }
 
 -- Terminal mode automatically enters insert mode
 autocmd("TermOpen", {
-  group = commonAutoGroup,
+  group = commanAugroup,
   command = "startinsert",
 })
 
 autocmd("BufEnter", {
-  group = commonAutoGroup,
+  group = commanAugroup,
   callback = function()
     vim.opt.formatoptions = vim.opt.formatoptions - "o" + "r"
   end,
@@ -22,7 +22,7 @@ autocmd("BufEnter", {
 
 -- Auto enable parinfer for lisp-like languages
 autocmd({ "BufEnter", "BufWinEnter" }, {
-  group = commonAutoGroup,
+  group = commanAugroup,
   pattern = lispFiletypes,
   desc = "Auto enable brackets matching for lisp files",
   command = "ParinferOn",
@@ -30,10 +30,30 @@ autocmd({ "BufEnter", "BufWinEnter" }, {
 
 -- Auto disable side line number for some filetypes
 autocmd("FileType", {
-  group = commonAutoGroup,
+  group = commanAugroup,
   pattern = { "nvim-docs-view" },
   desc = "Auto disable side line number for some filetypes",
   callback = function()
     vim.opt.number = false
   end,
+})
+
+autocmd("TextYankPost", {
+  group = commanAugroup,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  desc = "Highlight on yank",
+})
+
+autocmd("BufReadPost", {
+  group = commanAugroup,
+  callback = function()
+    local mark = vim.api.nvim_buf_get_mark(0, '"')
+    local lcount = vim.api.nvim_buf_line_count(0)
+    if mark[1] > 0 and mark[1] <= lcount then
+      pcall(vim.api.nvim_win_set_cursor, 0, mark)
+    end
+  end,
+  desc = "Go to last loc when opening a buffer",
 })
