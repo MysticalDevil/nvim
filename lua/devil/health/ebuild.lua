26780172bc42bb5d6f3badb9d0b8f13f7d0f9db5
MysticalDevil <jinpeng7981@outlook.com>
2026-01-21 15:58:47 +0800

    feat: add non-Gentoo judge helpers

--- lua/devil/health/ebuild.lua
+++ lua/devil/health/ebuild.lua
@@ -1,5 +1,13 @@
 local M = {}
 
+local function fs_exists(p)
+  return vim.uv.fs_stat(p) ~= nil
+end
+
+local function is_gentoo()
+  return fs_exists("/etc/gentoo-release") or fs_exists("/etc/portage") or fs_exists("/usr/bin/emerge")
+end
+
 local function exepath(bin)
   local p = vim.fn.exepath(bin)
   if p ~= nil and p ~= "" then
@@ -24,18 +32,26 @@ function M.check()
   local h = vim.health
   h.start("devil: ebuild")
 
-  local pkgdev = find_bin("pkgdev")
-  if pkgdev then
-    h.ok("pkgdev: " .. pkgdev)
-  else
-    h.error("pkgdev missing, Gentoo package app-portage/pkgdev")
-  end
+  if is_gentoo() then
+    do
+      local pkgdev = find_bin("pkgdev")
+      if pkgdev then
+        h.ok("pkgdev: " .. pkgdev)
+      else
+        h.error("pkgdev missing, Gentoo package app-portage/pkgdev")
+      end
+    end
 
-  local pkgcheck = find_bin("pkgcheck")
-  if pkgcheck then
-    h.ok("pkgcheck: " .. pkgcheck)
+    do
+      local pkgcheck = find_bin("pkgcheck")
+      if pkgcheck then
+        h.ok("pkgcheck: " .. pkgcheck)
+      else
+        h.error("pkgcheck missing, Gentoo package app-portage/pkgcheck")
+      end
+    end
   else
-    h.error("pkgcheck missing, Gentoo package app-portage/pkgcheck")
+    h.info("non-Gentoo system detected, skip pkgdev/pkgcheck checks")
   end
 
   local shellcheck = find_bin("shellcheck")
