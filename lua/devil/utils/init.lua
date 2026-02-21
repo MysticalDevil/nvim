9870d8b68bb3ebb1c837ebce5ff76e1086078861
MysticalDevil <jinpeng7981@outlook.com>
2026-02-21 19:50:15 +0800

    refactor(utils): prevent silent export overrides in aggregator

--- lua/devil/utils/init.lua
+++ lua/devil/utils/init.lua
@@ -1,20 +1,42 @@
 ---Aggregated utility module.
 ---@module devil.utils
 
-local M = {}
-local merge_tb = vim.tbl_deep_extend
-
 local common = require("devil.utils.common")
 local lsp_tool = require("devil.utils.lsp_tool")
 local command = require("devil.utils.command")
 local notify = require("devil.utils.notify")
 
--- Merge utility namespaces into a single export table.
-local temp_tb = merge_tb("force", {}, common or {})
-temp_tb = merge_tb("force", temp_tb, lsp_tool or {})
-temp_tb = merge_tb("force", temp_tb, command or {})
-temp_tb = merge_tb("force", temp_tb, notify or {})
+local M = {
+  common = common or {},
+  lsp_tool = lsp_tool or {},
+  command = command or {},
+  notify = notify or {},
+}
 
-M = temp_tb or {}
+local collisions = {}
+
+local function export_module(module_name, module)
+  for k, v in pairs(module) do
+    if M[k] == nil then
+      M[k] = v
+    elseif M[k] ~= v then
+      table.insert(collisions, ("%s.%s"):format(module_name, k))
+    end
+  end
+end
+
+export_module("common", M.common)
+export_module("lsp_tool", M.lsp_tool)
+export_module("command", M.command)
+export_module("notify", M.notify)
+
+if #collisions > 0 then
+  vim.schedule(function()
+    vim.notify(
+      ("utils export collisions ignored (kept first definition): %s"):format(table.concat(collisions, ", ")),
+      vim.log.levels.WARN
+    )
+  end)
+end
 
 return M
