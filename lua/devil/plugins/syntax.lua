f4c5f20ed22c3809c800c7f3ad02417c4d3a14ac
MysticalDevil <jinpeng7981@outlook.com>
2026-04-23 23:39:27 +0800

    fix(plugins): restore branch = main for nvim-treesitter-textobjects
    The default branch (master) is frozen and incompatible with current
    nvim-treesitter. The main branch is a full rewrite with a different
    file structure and API.

--- lua/devil/plugins/syntax.lua
+++ lua/devil/plugins/syntax.lua
@@ -47,6 +47,7 @@ return {
     dependencies = {
       {
         "nvim-treesitter/nvim-treesitter-textobjects",
+        branch = "main",
         init = function()
           vim.g.no_plugin_maps = true
         end,
